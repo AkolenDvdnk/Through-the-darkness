b2c7b2c13e6fc836a091f1b48169e9b234fa4bc9
AkolenDvdnk <ak0lenfs@gmail.com>
2022-03-15 14:59:44 +0200

    SpawnStone glow - proto

---
+++ Through the darkness/Assets/SpawnStone.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnStone : MonoBehaviour
+{
+    [SerializeField] GameObject glow;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            glow.SetActive(true);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            glow.SetActive(false);
+        }
+    }
+}
